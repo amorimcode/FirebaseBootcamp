f85068f3c1face2a56add402b7060bf764b20c68
Bruno Amorim <bruno.amorim@bankeiro.com.br>
2024-02-12 23:43:41 -0300

    Initial Commit

---
+++ FirebaseBootcamp/FirebaseBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseBootcampApp.swift
+//  FirebaseBootcamp
+//
+//  Created by Bruno Amorim on 12/02/24.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
